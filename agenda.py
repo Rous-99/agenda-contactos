42e20276758e50df1ceabf345def125dbd98e83d
Rous-99 <rous.bf.10@gmail.com>
2022-12-01 18:44:52 +0100

    funcion guardar contacto y archivar empezada

--- agenda.py
+++ agenda.py
@@ -6,7 +6,7 @@ from properties import *
 
 ###CREAMOS LAS FUNCIONES QUE VAMOS A NECESITAR###
 
-
+lista_usuarios=[] #defino una lista vacia donde se van a ir guardando los usuarios
 
 def eliminarContacto():
     pass
@@ -14,8 +14,33 @@ def eliminarContacto():
 def editarContacto():
     pass
 
+
+def archivarContacto():
+    prefijoArchivo="user"
+    nombreArchivo= prefijoArchivo + ".txt"
+    
+    with open(nombreArchivo, 'w') as user: #como cambio el nombre para que se vayan cambiando nuevos archivos?
+        lista_usuarios.sort() #ordeno la lista
+        for elemento in lista_usuarios:
+            user.write(elemento +"\n")
+    # archivo=open("user.txt", "w") #abro el archivo y escribo en el
+    
+    user.close()
+
 def guardarContacto(): #para crear el arhivo de txt de cada usuario
-    pass
+
+    ###CON LOS GETS SACO EL VALOR DE LAS VARIABLES###
+   name=nombre.get()
+   nombre_usuario=username.get()
+   correo=email.get()
+   pagina_web=paginaWeb.get()
+
+   lista_usuarios.append(name+"/" +nombre_usuario + "/" + correo + "/" + pagina_web) #separo con $ para visualizar mejor la lista
+   archivarContacto()
+   print(name, nombre_usuario,correo, pagina_web)
+   print(lista_usuarios)
+    #insertar codigo que haga que cuando guardo el contacto se cree un archivo nuevo con esos datos
+   
 
 
 
@@ -33,44 +58,44 @@ paginaWeb=tkinter.StringVar()
 
 ###CREAMOS LAS ETIQUETAS###
 
-tituloEtiqueta=tkinter.Label(root,text="AGENDA DE USUARIOS",font=(FONT_STYLE, FONT_SIZE, FONT_WEIGHT), fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-tituloEtiqueta.grid(row=0, column=1)
+tituloEtiqueta=tkinter.Label(root,text="AGENDA DE USUARIOS",font=(FONT_STYLE, FONT_SIZE, FONT_WEIGHT), fg=BG_BUTTON)
+tituloEtiqueta.grid(row=0, column=1, padx=10, pady=10, columnspan=2)
 
-nombreEtiqueta=tkinter.Label(root,text="Nombre", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-nombreEtiqueta.grid(row=1, column=0)
+nombreEtiqueta=tkinter.Label(root,text="Nombre", font=(FONT_STYLE,FONT_SIZE_LABEL),fg=LETTER_COLOR)
+nombreEtiqueta.grid(row=1, column=0, padx=20, pady=10)
 
-usernameEtiqueta=tkinter.Label(root, text="Username", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-usernameEtiqueta.grid(row=2, column=0)
+usernameEtiqueta=tkinter.Label(root, text="Username",font=(FONT_STYLE,FONT_SIZE_LABEL), fg=LETTER_COLOR)
+usernameEtiqueta.grid(row=2, column=0, padx=20, pady=10)
 
-emailEtiqueta=tkinter.Label(root, text="Email", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-emailEtiqueta.grid(row=3, column=0)
+emailEtiqueta=tkinter.Label(root, text="Email",font=(FONT_STYLE,FONT_SIZE_LABEL), fg=LETTER_COLOR)
+emailEtiqueta.grid(row=3, column=0, padx=20, pady=10)
 
 
-paginaWebEtiqueta=tkinter.Label(root, text="Pagina Web", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-paginaWebEtiqueta.grid(row=4, column=0)
+paginaWebEtiqueta=tkinter.Label(root, text="Pagina Web",font=(FONT_STYLE,FONT_SIZE_LABEL), fg=LETTER_COLOR)
+paginaWebEtiqueta.grid(row=4, column=0, padx=20, pady=10)
 
 # eliminarContactoEtiqueta= tkinter.Label(root, text="Eliminar contacto", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
 # eliminarContactoEtiqueta.grid(row=5, column=1)
 
 ###CREAMOS LAS ENTRADAS###
 
-nombreEntrada=tkinter.Entry(root, textvariable=nombre)
-nombreEntrada.grid(row=1, column=1)
+nombreEntrada=tkinter.Entry(root, textvariable=nombre, font=(FONT_STYLE, 15))
+nombreEntrada.grid(row=1, column=1, ipadx=100, ipady=5, columnspan=2)
 
-usernameEntrada=tkinter.Entry(root, textvariable=username)
-usernameEntrada.grid(row=2, column=1)
+usernameEntrada=tkinter.Entry(root, textvariable=username, font=(FONT_STYLE, 15))
+usernameEntrada.grid(row=2, column=1, ipadx=100, ipady=5, columnspan=2)
 
-emailEntrada=tkinter.Entry(root, textvariable=email)
-emailEntrada.grid(row=3, column=1)
+emailEntrada=tkinter.Entry(root, textvariable=email, font=(FONT_STYLE, 15))
+emailEntrada.grid(row=3, column=1, ipadx=100, ipady=5, columnspan=2)
 
-paginaWebEntrada=tkinter.Entry(root, textvariable=paginaWeb)
-paginaWebEntrada.grid(row=4, column=1)
+paginaWebEntrada=tkinter.Entry(root, textvariable=paginaWeb, font=(FONT_STYLE, 15))
+paginaWebEntrada.grid(row=4, column=1, ipadx=100, ipady=5, columnspan=2)
 
 ###CREAMOS LOS BOTONES###
-botonGuardar=tkinter.Button(root, text="Guardar contacto", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-botonEliminar=tkinter.Button(root, text="Eliminar contacto", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-botonGuardar.grid(row=5, column=1)
-botonEliminar.grid(row=6, column=1)
+botonGuardar=tkinter.Button(root, text="GUARDAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=guardarContacto)
+botonEliminar=tkinter.Button(root, text="ELIMINAR CONTACTO",font=(FONT_STYLE,15, FONT_WEIGHT), fg=LETTER_BUTTON, bg=BG_BUTTON)
+botonGuardar.grid(row=5, column=1, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
+botonEliminar.grid(row=6, column=1, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
 
 
 
