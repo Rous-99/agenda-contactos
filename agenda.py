f0d817c88e880cf65184bc53542741b86aa1016e
Rous-99 <rous.bf.10@gmail.com>
2022-12-07 13:16:37 +0100

    ver contactos añadido

--- agenda.py
+++ agenda.py
@@ -1,15 +1,99 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 # from tkinter import INSERT
 from properties import *
+from os import remove
 
-# i=0
 ###CREAMOS LAS FUNCIONES QUE VAMOS A NECESITAR###
 
 lista_usuarios=[] #defino una lista vacia donde se van a ir guardando los usuarios
 
-def eliminarContacto():
-    pass
+
+def consultarAgenda():
+    #crear una nueva ventana
+    newWindow=Tk()
+    newWindow.geometry(SIZE_SECOND_WD)
+    newWindow.title("Consulta de Usarios")
+
+    
+   
+    tv=ttk.Treeview(newWindow, columns=("Nombre", "Username", "Correo electrónico", "Página Web")) #el nombre de la columna se refiere no solo al encabezado, sino a todos los datos de esa columna
+    tv.pack(fill=BOTH, expand=True)
+    tv.column("#0", width=80)
+    tv.column("Nombre", width=80, anchor=CENTER)
+    
+   
+   
+    #Add some style:
+    # style = ttk.Style()
+
+    # style.theme_use("clam")
+
+    # style.configure("Treeview",
+    #                 background="silver",
+    #                 foreground="black",
+    #                 rowheight=55,
+    #                 fieldbackground="silver")
+
+
+    ##encabezados##
+    tv.heading("#0", text="Archivo", anchor=CENTER)
+    tv.heading("Nombre", text="Nombre", anchor=CENTER)
+    tv.heading("Username", text="Username", anchor=CENTER)
+    tv.heading("Correo electrónico", text="Correo electrónico", anchor=CENTER)
+    tv.heading("Página Web", text="Página Web", anchor=CENTER)
+
+    '''
+    La manera de insertar valores en la tabla:
+
+        tv.insert("", END, text="", values=(28,2)) se le puede pasar una variable, el texto va en la primera columna, con values se ponen las demas columnas
+    '''
+
+    with open("contactos.txt", "r") as verContactos:
+        for linea in verContactos:         #por cada linea tengo que hacer un split en las comas y guardar en variables los datos de cada contacto
+            print(linea)
+            linea_nueva=linea.replace('/', ',')
+            print(linea_nueva)
+            if linea=="\n":
+                continue #no quiero que haga nada
+            else:
+                nombreE, usernameE,correoE,paginaWebE, archivoE=linea_nueva.strip().split(",") #cuando elimino un contacto no me puede crear las variables porque se borra el espacio y se queda sin comas
+                tv.insert("", END, text=archivoE, values=(nombreE,usernameE,correoE,paginaWebE))
+            
+           
+        
+    
+
+
+    mainloop()
+
+def eliminarContacto(archivo_contacto, datos_contacto):
+    #necesito que al buscar el contacto por el nombre, me busque el archivo en el que están guardado sus datos y me borre ese archivo INDIVIDUAL. Luego, que en el archivo contactos me borre la linea en la que estaba con sus datos. 
+
+    with open("contactos.txt", "r") as contactos:
+        lines=contactos.readlines()
+        print(lines)
+        busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n" #este salto de linea se aplica de verdad?
+        print("busco esto: " + busqueda_contacto)
+        indice_contacto=lines.index(busqueda_contacto)
+        print(indice_contacto)
+        lines[indice_contacto]="\n" #lo dejo vacio para que el contacto se borre en la linea correspondiente
+        print(lines)
+        
+     
+    
+    with open("contactos.txt", "w") as contactos_edit:
+        for i in lines:
+            contactos_edit.write(i)
+        
+    ##ARCHIVO INDIVIDUAL##
+
+    remove(archivo_contacto) #así me borra el archivo del directorio
+
+    messagebox.showinfo("Eliminado", "Contacto eliminado con éxito")    
+
+
 
 
 # def consultarAgenda():
@@ -46,22 +130,27 @@ def archivarContacto():
         with open("contactos.txt", "a") as contacto:
             contacto.write(contacto_nuevo +"/" + nombreArchivo + "\n")
     user.close()
-    
+        
 
 def guardarContacto(): #para crear el arhivo de txt de cada usuario
 
     ###CON LOS GETS SACO EL VALOR DE LAS VARIABLES###
-   name=nombre.get()
-   nombre_usuario=username.get()
-   correo=email.get()
-   pagina_web=paginaWeb.get()
+    name=nombre.get()
+    nombre_usuario=username.get()
+    correo=email.get()
+    pagina_web=paginaWeb.get()
 
-   print(name)
+    if "@" in correo: #validar el correo, si tiene "@"
+        lista_usuarios.append(name+"," +nombre_usuario + "," + correo + "," + pagina_web) #separo con $ para visualizar mejor la lista, la lista me sirve para añadir contactos a contactos.txt, pero se borra cuando cierro la app, asi que debo almacenar los contactos en txt
+        archivarContacto()
+        messagebox.showinfo("Guardado", "Contacto guardado con éxito")
+    else:
+        messagebox.showinfo("ERROR", "El correo electrónico debe contener un '@'")
 
-   lista_usuarios.append(name+"," +nombre_usuario + "," + correo + "," + pagina_web) #separo con $ para visualizar mejor la lista, la lista me sirve para añadir contactos a contactos.txt, pero se borra cuando cierro la app, asi que debo almacenar los contactos en txt
-   archivarContacto()
-#    print(name, nombre_usuario,correo, pagina_web)
-#    print(lista_usuarios)
+
+
+        
+
 
 def editarContacto(archivo_contacto, datos_contacto):
     # #   ###CON LOS GETS SACO EL VALOR DE LAS VARIABLES###
@@ -70,36 +159,34 @@ def editarContacto(archivo_contacto, datos_contacto):
     correo=email.get()
     pagina_web=paginaWeb.get()
 
-    with open("contactos.txt", "r") as contactos:
-        lines=contactos.readlines()
-        print(lines)
-        busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n" #este salto de linea se aplica de verdad?
-        print("busco esto: " + busqueda_contacto)
-        indice_contacto=lines.index(busqueda_contacto)
-        print(indice_contacto)
-        lines[indice_contacto]=name+"," +nombre_usuario + "," + correo + "," + pagina_web+"/"+archivo_contacto+"\n"
+    if "@" in correo:
         
-     
+        with open("contactos.txt", "r") as contactos:
+            lines=contactos.readlines()
+            print(lines)
+            busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n" #este salto de linea se aplica de verdad?
+            print("busco esto: " + busqueda_contacto)
+            indice_contacto=lines.index(busqueda_contacto)
+            print(indice_contacto)
+            lines[indice_contacto]=name+"," +nombre_usuario + "," + correo + "," + pagina_web+"/"+archivo_contacto+"\n"
+        
+        with open("contactos.txt", "w") as contactos_edit:
+            for i in lines:
+                contactos_edit.write(i)
     
-    with open("contactos.txt", "w") as contactos_edit:
-        # contactos_edit.write("me deberia sustituir todo")
-        for i in lines:
-            contactos_edit.write(i)
-        # for contacto in lines:
-        #     contactos_edit.write(contacto + "\n")
-
+        with open(archivo_contacto, "w") as archivoEditado:
             
-    ###filename (input), line__number(input), text=input
-
-    with open(archivo_contacto, "w") as archivoEditado:
+            archivoEditado.write(name +"," +nombre_usuario + "," + correo + "," + pagina_web)#quiero escribir lo que haya actualmente en el entry no?
         
-        archivoEditado.write(name +"," +nombre_usuario + "," + correo + "," + pagina_web)#quiero escribir lo que haya actualmente en el entry no?
+        messagebox.showinfo("Editado", "Contacto editado con éxito")    
+    else:
+        messagebox.showinfo("ERROR", "El correo debe contener un '@")
 
-  
-    
+   
 
 
-def buscarContacto():
+
+def buscarContactoEdit():
     contacto=contact.get() #para buscar por el nombre
     print(contacto)
     ##LEER EL ARCHIVO CONTACTOS###
@@ -108,12 +195,22 @@ def buscarContacto():
         if contacto in linea: #como hago para que deje de leer las siguientes lineas si ya ha encontrado el nombre?
             datos_contacto, archivo_contacto=linea.strip().split("/")
             print("Lo he encontrado")
-            # print(datos_contacto)
             print(archivo_contacto) 
             editarContacto(archivo_contacto, datos_contacto) #solo si existe el contacto quiero que me lo edite
-        else:
-            print("No existe el contacto")
+    archivo_contactos.close()    
     
+def buscarContactoElim():
+    contacto=contact.get() #para buscar por el nombre
+    print(contacto)
+    ##LEER EL ARCHIVO CONTACTOS###
+    archivo_contactos=open("contactos.txt", "r")
+    for linea in archivo_contactos: #esto me lee cada linea del archivo
+        if contacto in linea: #como hago para que deje de leer las siguientes lineas si ya ha encontrado el nombre?
+            datos_contacto, archivo_contacto=linea.strip().split("/")
+            print("Lo he encontrado")
+            print(archivo_contacto) 
+            eliminarContacto(archivo_contacto, datos_contacto) #solo si existe el contacto quiero que me lo edite
+    archivo_contactos.close()    
 
     
 
@@ -150,11 +247,12 @@ emailEtiqueta.grid(row=3, column=0, padx=20, pady=10)
 paginaWebEtiqueta=Label(root, text="Pagina Web",font=(FONT_STYLE,FONT_SIZE_LABEL), fg=LETTER_COLOR)
 paginaWebEtiqueta.grid(row=4, column=0, padx=20, pady=10)
 
-buscarContactoEtiqueta=Label(root, text="Nombre del contacto a buscar",font=(FONT_STYLE,FONT_SIZE_LABEL), fg=LETTER_COLOR)
+buscarContactoEtiqueta=Label(root, text="Buscar contacto por nombre",font=(FONT_STYLE,FONT_SIZE_LABEL), fg=LETTER_COLOR)
 buscarContactoEtiqueta.grid(row=7, column=1, padx=20, pady=10)
 
-# eliminarContactoEtiqueta= tkinter.Label(root, text="Eliminar contacto", fg=LETTER_COLOR, bg=BACKGROUND_COLOR)
-# eliminarContactoEtiqueta.grid(row=5, column=1)
+###tkinter.messagebox.showinfo("titulo", "texto")
+
+
 
 ###CREAMOS LAS ENTRADAS###
 
@@ -176,15 +274,12 @@ contactoBuscado.grid(row=8, column=1, ipadx=100, ipady=5, columnspan=2)
 
 ###CREAMOS LOS BOTONES###
 botonGuardar=Button(root, text="GUARDAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=guardarContacto)
-# botonEliminar=tkinter.Button(root, text="EDITAR CONTACTO",font=(FONT_STYLE,15, FONT_WEIGHT), fg=LETTER_BUTTON, bg=BG_BUTTON)
-botonGuardar.grid(row=1, column=3, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
-# botonEliminar.grid(row=2, column=3, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
-botonBuscar=Button(root, text="BUSCAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=buscarContacto)
-botonBuscar.grid(row=3, column=3, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
-# botonEditar=tkinter.Button(root, text="EDITAR CONTACTO",font=(FONT_STYLE,15, FONT_WEIGHT), fg=LETTER_BUTTON, bg=BG_BUTTON)
-# botonEditar.grid(row=4, column=3, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
-
-
-
+botonEliminar=Button(root, text="ELIMINAR CONTACTO",font=(FONT_STYLE,15, FONT_WEIGHT), fg=LETTER_BUTTON, bg=BG_BUTTON, command=buscarContactoElim)
+botonGuardar.grid(row=2, column=3, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
+botonAgenda=Button(root,text="VER AGENDA", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=consultarAgenda)
+botonAgenda.grid(row=3, column=3, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
+botonEliminar.grid(row=10, column=1, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
+botonEditar=Button(root, text="EDITAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=buscarContactoEdit)
+botonEditar.grid(row=9, column=1, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
 
 root.mainloop() #va a registrar todo lo que ocurre mientras la ventana está abierta
