5df4b020b2e0e216b85db7f027dbb9a373f251c9
Rous-99 <rous.bf.10@gmail.com>
2022-12-09 11:29:22 +0100

    optimizacion hecha
    falta meter todos los archivos individuales en una carpeta y que se vayan añadiendo ahi

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # agenda-contactos
- 
+Si se quiere probar la app como si la agenda estuviese vacía, hay que colocar en "suma.txt" el número en 1 y en "contactos.txt" borrar todos los datos y colocarse en la línea 1. Además de eliminar los ficheros individuales que se hayan creado anteriormente.
+Para validar el correo hice uso de re y para la interfaz gráfica usé la librería Tkinter.
--- agenda.py
+++ agenda.py
@@ -1,15 +1,20 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-# from tkinter import INSERT
 from properties import *
 from os import remove
+import re
 
 
 # ###CREAMOS LAS FUNCIONES QUE VAMOS A NECESITAR###
 
-lista_usuarios=[] #defino una lista vacia donde se van a ir guardando los usuarios
+lista_usuarios=[] #defino una lista vacia donde se van a ir guardando los usuarios cada vez que abramos la interfaz
 
+def validarCorreo(arg_email):
+    is_valid=True
+    EMAIL_REGEX=re.compile(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
+    is_valid=False if not EMAIL_REGEX.match(arg_email) else True
+    return is_valid
 
 def mostrarInstrucciones():
     instrucciones=messagebox.showinfo("Instrucciones de uso", "Hola! Soy tu agenda y quiero que aprendas a usarme correctamente :D. EDITAR CONTACTO: si quieres editar un contacto tienes que introducir su nombre en 'Buscar contacto por su nombre' y rellenar todos los campos del contacto con los cambios que quieras, eso sí, dale al botón de 'Editar Contacto' ÚNICAMENTE después de haber realizado esto. ELIMINAR CONTACTO: para eliminar un contacto debes introducir su nombre en 'Buscar contacto por su nombre' y darle al botón de 'Eliminar Contacto'. Eso es todo!! Gracias y espero que te sea útil <3 ")
@@ -23,19 +28,21 @@ def consultarAgenda():
 
     ##CREO EL ESTILO QUE QUIERO APLICAR###
     style = ttk.Style()
-    style.configure("mystyle1.Treeview", highlightthickness=1, rowheight=40, bd=0, font=('Georgia', 12)) # Modify the font of the body
-    style.configure("mystyle1.Treeview.Heading", font=('Calibri', 15,'bold'),foreground=BG_BUTTON) # Modify the font of the headings
-    style.layout("mystyle1.Treeview", [('mystyle1.Treeview.treearea', {'sticky': 'nswe'})]) # Remove the borders
-
+    style.configure("mystyle1.Treeview", highlightthickness=1, rowheight=40, bd=0, font=('Georgia', 12)) #modifico el cuerpo de treeview
+    style.configure("mystyle1.Treeview.Heading", font=('Calibri', 15,'bold'),foreground=BG_BUTTON) #modifico el heading
+    style.layout("mystyle1.Treeview", [('mystyle1.Treeview.treearea', {'sticky': 'nswe'})]) #quito los bordes
 
     tv=ttk.Treeview(newWindow,style="mystyle1.Treeview", columns=("Nombre", "Username", "Correo electrónico", "Página Web")) #el nombre de la columna se refiere no solo al encabezado, sino a todos los datos de esa columna
-    tv.column("#0", width=80)
+    
+    ###CENTRO LOS DATOS DE CADA COLUMNA##
+    tv.column("#0", width=80, anchor=CENTER)
     tv.column("Nombre", width=80, anchor=CENTER)
+    tv.column("Username", width=80, anchor=CENTER)
+    tv.column("Correo electrónico", width=80, anchor=CENTER)
+    tv.column("Página Web", width=80, anchor=CENTER)
     tv.pack(fill=BOTH, expand=True)
     
     tv.tag_configure("row", background=BACKGROUND_COLOR, font=("Georgia",10))
-
-
 
     ##encabezados##
     tv.heading("#0", text="Archivo", anchor=CENTER)
@@ -45,80 +52,77 @@ def consultarAgenda():
     tv.heading("Página Web", text="Página Web", anchor=CENTER)
 
     '''
-    La manera de insertar valores en la tabla:
-
-        tv.insert("", END, text="", values=(28,2)) se le puede pasar una variable, el texto va en la primera columna, con values se ponen las demas columnas
+    Para insertar los datos recolecto éstos del archivo contactos.txt. Leo cada línea, que tiene los datos de cada contacto. Separo cada dato y lo almaceno en una variable cada uno. Inserto cada variable en la tabla.
+    Cuando un contacto se ha eliminado queda una linea vacia en contactos.txt. Si leyendo las lineas se encuentra unicamente "\n" no quiero que me lo añada, sino que siga leyendo.
     '''
 
     with open("contactos.txt", "r") as verContactos:
-        for linea in verContactos:         #por cada linea tengo que hacer un split en las comas y guardar en variables los datos de cada contacto
-            print(linea)
-            linea_nueva=linea.replace('/', ',')
-            print(linea_nueva)
+        for linea in verContactos:        
+            linea_nueva=linea.replace('/', ',') #para separar el nombre del archivo cambio / por una ,
             if linea=="\n":
                 continue #no quiero que haga nada
             else:
-                nombreE, usernameE,correoE,paginaWebE, archivoE=linea_nueva.strip().split(",") #cuando elimino un contacto no me puede crear las variables porque se borra el espacio y se queda sin comas
+                nombreE, usernameE,correoE,paginaWebE, archivoE=linea_nueva.strip().split(",")
                 tv.insert("", END,tags="row", text=archivoE, values=(nombreE,usernameE,correoE,paginaWebE))
             
-           
-        
-    
-
 
     mainloop()
 
-def eliminarContacto(archivo_contacto, datos_contacto):
-    #necesito que al buscar el contacto por el nombre, me busque el archivo en el que están guardado sus datos y me borre ese archivo INDIVIDUAL. Luego, que en el archivo contactos me borre la linea en la que estaba con sus datos. 
-
-    with open("contactos.txt", "r") as contactos:
+def eliminarContacto():
+   '''
+    Mismo procedimiento que para editar un contacto, solo que en la lista en vez de enviar los nuevos datos, lo sustituimos por "\n" para que al escribirlo en el archivo no se muestre
+   '''
+   contacto=buscarContacto()#buscamos contacto y obtenemos una tupla
+   datos_contacto, archivo_contacto=contacto #separamos cada elemento de la tupla y lo almacenamos en estas variables
+   
+   
+   with open("contactos.txt", "r") as contactos:
         lines=contactos.readlines()
-        print(lines)
-        busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n" #este salto de linea se aplica de verdad?
-        print("busco esto: " + busqueda_contacto)
+        busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n"
         indice_contacto=lines.index(busqueda_contacto)
-        print(indice_contacto)
         lines[indice_contacto]="\n" #lo dejo vacio para que el contacto se borre en la linea correspondiente
-        print(lines)
-        
-     
     
-    with open("contactos.txt", "w") as contactos_edit:
+   with open("contactos.txt", "w") as contactos_edit:
         for i in lines:
             contactos_edit.write(i)
         
     ##ARCHIVO INDIVIDUAL##
+   remove(archivo_contacto) #así me borra el archivo del directorio
 
-    remove(archivo_contacto) #así me borra el archivo del directorio
-
-    messagebox.showinfo("Eliminado", "Contacto eliminado con éxito")    
+   messagebox.showinfo("Eliminado", "Contacto eliminado con éxito")  #muestro un mensaje cuando el contacto se ha eliminado con éxito
 
 
 def archivarContacto():
+    '''
+    Para poder almacenar cada contacto en un archivo diferente con el formato: "user_.txt" cambiando el _ por el número que corresponda, creo un archivo "suma.txt" que va a almacenar un número, a partir del 1, y se la va a ir sumando 1 cada vez que un contacto se haya guardado. El nº que esté almacenado en ese archivo será el correspondiente al que va en el nombre del archivo individual de ese contacto.
+    '''
     ###ABRO EL ARCHIVO SUMA PARA EXTRAER EL NÚMERO###
-    archivo_suma=open("suma.txt", "r")
-    num=archivo_suma.read()
-    print(num)
-    archivo_suma.close()
+        #manualmente lo inicializo con un 1
+    with open("suma.txt", "r") as archivo_suma:
+        num=archivo_suma.read()
+
     ###NOMBRO EL ARCHIVO CON ESE NÚMERO###
-    nombreArchivo= "user"+ str(num)+ ".txt"
+    nombreArchivo= "user"+ str(num)+ ".txt" #formato de cada archivo
+
     ###SOBREESCRIBO EN EL ARCHIVO EL NÚMERO SUMÁNDOLE 1###
-    num_act=open("suma.txt", "w")
-    num=int(num)+1
-    num_act.write(str(num))
-    num_act.close()
+    with open("suma.txt", "w") as numeroActualizado:
+        num=int(num)+1
+        numeroActualizado.write(str(num))
+ 
     ###CREO UN ARCHIVO CON EL NOMBRE ACTUALIZADO Y GUARDO LOS DATOS###
+    contacto_nuevo=lista_usuarios[-1]
+
     with open(nombreArchivo, 'w') as user: 
         ###AÑADO AL ARCHIVO CREADO UNICAMENTE EL ÚLTIMO ELEMENTO DE LA LISTA, QUE ES EL NUEVO CONTACTO AGREGADO###
-        contacto_nuevo=lista_usuarios[-1] #lista de caracteres
-        print(contacto_nuevo)
         user.write(contacto_nuevo)
-        with open("contactos.txt", "a") as contacto:
-            contacto.write(contacto_nuevo +"/" + nombreArchivo + "\n")
-    user.close()
+    
+    ##GUARDO ESE CONTACTO EN EL ARCHIVO CONTACTOS.TXT, QUE ALMANCENARÁ TODOS LOS CONTACTOS DE LA AGENDA###
+    with open("contactos.txt", "a") as contacto: #no quiero sobreeescribir sino añadir abajo, así que escribo con "a"
+        contacto.write(contacto_nuevo +"/" + nombreArchivo + "\n")
+    
         
 
-def guardarContacto(): #para crear el arhivo de txt de cada usuario
+def guardarContacto():
 
     ###CON LOS GETS SACO EL VALOR DE LAS VARIABLES###
     name=nombre.get()
@@ -126,85 +130,75 @@ def guardarContacto(): #para crear el arhivo de txt de cada usuario
     correo=email.get()
     pagina_web=paginaWeb.get()
 
-    if "@" in correo: #validar el correo, si tiene "@"
-        lista_usuarios.append(name+"," +nombre_usuario + "," + correo + "," + pagina_web) #separo con $ para visualizar mejor la lista, la lista me sirve para añadir contactos a contactos.txt, pero se borra cuando cierro la app, asi que debo almacenar los contactos en txt
+    check=validarCorreo(correo)
+
+    if check==True: #validar el correo
+        lista_usuarios.append(name+"," +nombre_usuario + "," + correo + "," + pagina_web) # la lista me sirve para añadir contactos a contactos.txt, pero se borra cuando cierro la app, asi que debo almacenar los contactos en txt
         archivarContacto()
         messagebox.showinfo("Guardado", "Contacto guardado con éxito")
     else:
-        messagebox.showinfo("ERROR", "El correo electrónico debe contener un '@'")
+        messagebox.showinfo("ERROR", "El correo electrónico no cumple con el formato. Revisalo por favor")
 
 
 
-        
+def editarContacto():
+    '''
+    Primero buscamos el contacto con la función buscarContacto(). Separamos la tupla que obtenemos en dos variables para almacenar los datos del contacto y el nombre del archivo de manera separada. Obtenemos los nuevos valores de los campos del contacto con el get. Solo si hay un "@" en el correo puedo editar el contacto. 
+    Para editar el contacto abrimos el archivo contactos. txt, leemos cada linea y almacenamos ésto en una variable llamada "lines", que es de tipo lista. Creo una variable "busqueda_contacto" que será el contacto que buscamos y unimos los datos con el formato correspondiente para poder encontrarla en "lines". Creo una variable "indice_contacto" que buscará en "lines" el índice donde se encunetra "busqueda_contacto".
+    Con este índice ya podemos cambiar en el lugar correspondiente los valores del contacto con los nuevos datos. 
 
+    Una vez esos valores se han escrito en la lista, abro el archivo contactos.txt para sobreescribir y por cada elemento de lines escribo en el archivo, con las modificaciones en el contacto ya hechas.
+    '''
 
-def editarContacto(archivo_contacto, datos_contacto):
-    # #   ###CON LOS GETS SACO EL VALOR DE LAS VARIABLES###
+    contacto=buscarContacto() #buscamos contacto y obtenemos una tupla
+    datos_contacto, archivo_contacto=contacto #separamos cada elemento de la tupla y lo almacenamos en estas variables
+
+    ###CON LOS GETS SACO EL VALOR DE LAS VARIABLES###
     name=nombre.get()
     nombre_usuario=username.get()
     correo=email.get()
     pagina_web=paginaWeb.get()
 
-    if "@" in correo:
+    check=validarCorreo(correo)
+
+
+    if check ==True:
         
         with open("contactos.txt", "r") as contactos:
-            lines=contactos.readlines()
-            print(lines)
-            busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n" #este salto de linea se aplica de verdad?
-            print("busco esto: " + busqueda_contacto)
-            indice_contacto=lines.index(busqueda_contacto)
-            print(indice_contacto)
-            lines[indice_contacto]=name+"," +nombre_usuario + "," + correo + "," + pagina_web+"/"+archivo_contacto+"\n"
+            lines=contactos.readlines() #obtengo una lista
+            busqueda_contacto=datos_contacto+"/"+archivo_contacto+"\n" #escribimos el contato con el formato de los contactos en la lista
+            indice_contacto=lines.index(busqueda_contacto) #busco el indice en la lista para este contacto
+            lines[indice_contacto]=name+"," +nombre_usuario + "," + correo + "," + pagina_web+"/"+archivo_contacto+"\n" #modifico en ese indice los valores del contacto
         
         with open("contactos.txt", "w") as contactos_edit:
-            for i in lines:
+            for i in lines: #escribo para cada elemento de la lista en el archivo
                 contactos_edit.write(i)
     
-        with open(archivo_contacto, "w") as archivoEditado:
+        with open(archivo_contacto, "w") as archivoEditado: #modifico el archivo indivual sobreescribiendo únicamente
             
-            archivoEditado.write(name +"," +nombre_usuario + "," + correo + "," + pagina_web)#quiero escribir lo que haya actualmente en el entry no?
+            archivoEditado.write(name +"," +nombre_usuario + "," + correo + "," + pagina_web)
         
-        messagebox.showinfo("Editado", "Contacto editado con éxito")    
+        messagebox.showinfo("Editado", "Contacto editado con éxito")    #muestro un mensaje cuando se ha editado correctamente
     else:
-        messagebox.showinfo("ERROR", "El correo debe contener un '@")
+        messagebox.showinfo("ERROR", "El correo electrónico no cumple con el formato. Revisalo por favor") #muestro el error sino se puede editar
 
-   
-
-
-
-def buscarContactoEdit():
+    
+def buscarContacto():
+    '''
+    Para editar o eliminar un contacto tenemos que comprobar que existe buscandolo. Creo una variable contacto que va a recibir el valor que el usuario busca por la interfaz. Abrimos el archivo contactos.txt y buscamos en cada linea por el nombre. Solo si existe, es decir, si el contacto está en alguna línea, vamos a separar en dos variables los datos. En una guardamos los datos del contacto y en otro el nombre del archivo en el que están guardados. Hacemos un return con esas variables para usarlas en la función de editar o eliminar un contacto.
+    '''
     contacto=contact.get() #para buscar por el nombre
-    print(contacto)
     ##LEER EL ARCHIVO CONTACTOS###
     archivo_contactos=open("contactos.txt", "r")
     for linea in archivo_contactos: #esto me lee cada linea del archivo
-        if contacto in linea: #como hago para que deje de leer las siguientes lineas si ya ha encontrado el nombre?
+        if contacto in linea:
             datos_contacto, archivo_contacto=linea.strip().split("/")
-            print("Lo he encontrado")
-            print(archivo_contacto) 
-            editarContacto(archivo_contacto, datos_contacto) #solo si existe el contacto quiero que me lo edite
-    archivo_contactos.close()    
-    
-def buscarContactoElim():
-    contacto=contact.get() #para buscar por el nombre
-    print(contacto)
-    ##LEER EL ARCHIVO CONTACTOS###
-    archivo_contactos=open("contactos.txt", "r")
-    for linea in archivo_contactos: #esto me lee cada linea del archivo
-        if contacto in linea: #como hago para que deje de leer las siguientes lineas si ya ha encontrado el nombre?
-            datos_contacto, archivo_contacto=linea.strip().split("/")
-            print("Lo he encontrado")
-            print(archivo_contacto) 
-            eliminarContacto(archivo_contacto, datos_contacto) #solo si existe el contacto quiero que me lo edite
-    archivo_contactos.close()    
-
-    
-
+            return datos_contacto, archivo_contacto
+    archivo_contactos.close()
 
 root=Tk() #creamos la ventana principal, por convención se llama root
 root.geometry(SIZE) #redimensionamos la ventana
 root.title("Agenda de usuarios")  #añadimos un titulo a la ventana
-
 
 ##CREAMOS LAS VARIABLES###
 nombre=StringVar()
@@ -236,8 +230,6 @@ buscarContactoEtiqueta=Label(root, text="Buscar contacto por nombre",font=(FONT_
 buscarContactoEtiqueta.grid(row=7, column=1, padx=20, pady=10)
 
 
-
-
 ###CREAMOS LAS ENTRADAS###
 
 nombreEntrada=Entry(root, textvariable=nombre, font=(FONT_STYLE, 15))
@@ -258,12 +250,12 @@ contactoBuscado.grid(row=8, column=1, ipadx=100, ipady=5, columnspan=2)
 
 ###CREAMOS LOS BOTONES###
 botonGuardar=Button(root, text="GUARDAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=guardarContacto)
-botonEliminar=Button(root, text="ELIMINAR CONTACTO",font=(FONT_STYLE,15, FONT_WEIGHT), fg=LETTER_BUTTON, bg=BG_BUTTON, command=buscarContactoElim)
+botonEliminar=Button(root, text="ELIMINAR CONTACTO",font=(FONT_STYLE,15, FONT_WEIGHT), fg=LETTER_BUTTON, bg=BG_BUTTON, command=eliminarContacto)
 botonGuardar.grid(row=2, column=4, ipadx=5, ipady=5, padx=25, pady=10, columnspan=2)
 botonAgenda=Button(root,text="      VER AGENDA      ", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=consultarAgenda)
 botonAgenda.grid(row=3, column=4, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
 botonEliminar.grid(row=10, column=1, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
-botonEditar=Button(root, text="EDITAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=buscarContactoEdit)
+botonEditar=Button(root, text="EDITAR CONTACTO", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=editarContacto)
 botonEditar.grid(row=9, column=1, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
 botonInstrucciones=Button(root, text="INSTRUCCIONES", font=(FONT_STYLE,15, FONT_WEIGHT),fg=LETTER_BUTTON, bg=BG_BUTTON, command=mostrarInstrucciones)
 botonInstrucciones.grid(row=4,column=4, ipadx=5, ipady=5, padx=10, pady=10, columnspan=2)
